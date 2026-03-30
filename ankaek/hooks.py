73ad2eb37f1618ca776aeb116747973767ea9948
glucklich <glucklich@pop-os.local>
2026-03-30 21:59:20 +0545

    fix: remove all fixtures, rely on after_migrate hook only

--- ankaek/hooks.py
+++ ankaek/hooks.py
@@ -253,8 +253,4 @@ brand_html = "ankaEK"
 app_logo_url = "/assets/ankaek/images/logo.jpg"
 login_logo_url = "/assets/ankaek/images/logo.jpg"
 favicon = "/assets/ankaek/images/favicon.ico"
-fixtures = [
-    {"dt": "Desktop Icon"},
-    {"dt": "Website Settings", "filters": [["name", "=", "Website Settings"]]},
-]
 after_migrate = ["ankaek.install.after_install"]
